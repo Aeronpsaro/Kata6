fa085b29eff5b571ec9145944a1ce96df1f602dd
equipo <equipo@DESKTOP-S4N862J>
2021-12-22 12:21:14 +0000

    Kata6-Versión2: Se ha añadido la clase Helicopter.

---
+++ src/toys/CAr.java
@@ -0,0 +1,26 @@
+package toys;
+
+public class Car {
+    private final Integer serialNumber;
+    private final String type = "car";
+
+    public Car(Integer serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public Integer getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+    
+    public void pack() {
+        System.out.printf("Packing '%s' '%d'\n", this.type, this.serialNumber);
+    }
+    
+    public void label() {
+        System.out.printf("Labelling '%s '%d'\n", this.type, this.serialNumber);
+    }
+}
